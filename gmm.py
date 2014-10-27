6572082cd8d099f9425cbe4e354db9c7d1d7b1d7
Simrat Singh Chhabra <simratsinghchhabra@gmail.com>
2014-10-27 22:12:00 +0530

    initial gmm

---
+++ gmm.py
@@ -0,0 +1,40 @@
+from numpy import *
+import pypr.clustering.gmm as gmm
+
+def load_data(path, meta):
+	x=empty((1500*100, 12))
+	f1 = open(meta, 'r')
+	nvec=0
+	for line in f1:
+		s=line.rstrip('\n')
+		f2=open(path+s, 'r')
+		for line in f2:
+			line=line.rstrip('\n')
+			coeff = line.split()
+			if(len(coeff)==12):
+				if(nvec>=150000):
+					x=append(x, coeff, axis=0)
+				x[nvec]=coeff
+				nvec+=1
+			else:
+				print "Not 12"
+				print len(coeff)
+
+		f2.close()
+	f1.close()
+	print nvec
+	return x
+
+
+
+path="/home/simrat/speechdata/english_digits/Training_data/cepstrals/"
+meta=path+"meta_data/ceplist.txt"
+
+x=load_data(path, meta)
+
+center_list, cov_list, p_k, logLL =gmm.em(X=x, K=16, max_iter=32, verbose=True)
+
+print center_list
+print cov_list
+print p_k
+print logLL
